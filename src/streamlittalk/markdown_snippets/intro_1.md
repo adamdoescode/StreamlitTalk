f48694b5bb9fd4a2c1c1a7f7a57dbbda9c927747
AdamG <grey.ham101@gmail.com>
2026-01-12 06:49:52 +0800

    add dash to see if I can. More intro slides written.

---
+++ src/streamlittalk/markdown_snippets/intro_1.md
@@ -0,0 +1,8 @@
+## What is streamlit?
+
+- Yet another python web app platform
+- Web apps for data scientists scared of React (me!)
+- Looks *nice* by default
+- Easy to install and run
+
+## Why tho?
--- src/streamlittalk/pages/intro.py
+++ src/streamlittalk/pages/intro.py
@@ -2,7 +2,11 @@ from pathlib import Path
 import streamlit as st
 
 FILE_DIR = Path(__file__)
+SNIPPETS_DIR = FILE_DIR.parents[1] / "markdown_snippets"
 
 if __name__ == "__main__":
     st.title("Introduction")
-    st.markdown((FILE_DIR.parents[1] / "markdown_snippets" / "intro.md").read_text())
+    st.markdown((SNIPPETS_DIR / "intro_1.md").read_text(), unsafe_allow_html=True)
+    # this must be relative to where we call streamlit run from
+    st.image(image="media/ytho.jpg", width=300)
+    st.markdown((SNIPPETS_DIR / "intro_2.md").read_text())
