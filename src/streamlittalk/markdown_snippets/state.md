21f43ca827eb4f3a4b16076553c791409a4a7fc4
AdamG <grey.ham101@gmail.com>
2026-01-26 16:04:13 +0800

    re-invented slideshow app within streamlit. Complete state discussion section

---
+++ src/streamlittalk/markdown_snippets/state.md
@@ -0,0 +1,80 @@
+---
+0
+## Powerpoint slides in Streamlit!
+
+- Welcome to a streamlit implementation of powerpoint slides!
+- To pull this off we need to make use of the web-app's "memory"
+  - as oppossed to just storing everything in python variables
+    - which are NOT stored between app refreshes!
+- Thus we now need to discuss the concept of **state**
+$$
+\text{state} = \text{memory (for web apps)}
+$$
+- Modern web frameworks involve modifying state instead of modifying HTML.
+- In streamlit state is stored in a special variable:
+```python
+st.session_state
+```
+- We'll talk a little about how session_state is implemented because its a nice dive into how to do class enum dict thingies.
+---
+1
+## Why we need to use *state*
+
+- A demonstration of why we want state...
+- We have two counter buttons:
+  1. button whose content is stored in a local variable `local_count`
+  2. buttonw whose content is stored in `st.session_state`
+- Let's see how each behaves and walk through the code
+
+---
+2
+## (Screaming) Why is it stuck at 1??!
+
+- The local counter is being reset *every time* the button is clicked, so that:
+  - `local_count` is set to 0
+  - we increment `local_count` by 1
+  - it gets printed.
+- Meanwhile, the chad `st.session_state.session_count` variable is not reset by the hidden page refresh that wipes clean our local python variables.
+
+---
+3
+## Session state implementation
+
+- Held in the streamlit `__init__.py` as:
+```python
+session_state = _SessionStateProxy()
+```
+- `_SessionStateProxy()` is a proxy for `SessionStateProxy()`:
+```python
+class SessionStateProxy(MutableMapping[Key, Any]):
+```
+- So its a `MutableMapping` with some custom dunder methods for streamlit's content
+- **Mapping** == a data structure of key value pairs; think a `dict` (or hash table).
+- **Mutable** == changeable - aka we can change the key - value pairs in this.
+
+---
+4
+## The trouble with `st.rerun` and why you should use "callbacks"
+
+- You saw a callback just before!
+- It was this bad boi:
+```python
+def reset_counter_state() -> None:
+    """This is a callback function."""
+    st.session_state.session_count = 0
+
+# using the `on_click` argument we can add the callback to our reset button
+st.button("Reset session counter", on_click=reset_counter_state)
+```
+- Here, we run the function `reset_counter_state` when the user clicks the `st.button`.
+- We can think of this as an "on demand function".
+- The original chatGPT suggestion was this:
+```python
+if st.button("Reset session counter"):
+    st.session_state.reset_counter_state = 0
+    st.rerun()
+```
+- this is bad!
+- although `st.rerun()` works in this case, it can often cause chaos when you have basically any level of complexity in your web app's state.
+- Users should beware that LLMs will use the `st.rerun` pattern unless you tell it not to.
+- Fortunately, since the slide tracking variable here `st.session_state.slide` is set as session state the `st.rerun()` does not reset our slide deck back to the start page.
--- src/streamlittalk/pages/state_and_caching.py
+++ src/streamlittalk/pages/state_and_caching.py
@@ -2,3 +2,121 @@
 Reinventing powerpoint slides in Streamlit:
     An introduction to how streamlit allows control of state
 """
+
+from pathlib import Path
+
+import streamlit as st
+
+
+def read_markdown_content() -> dict[str, str]:
+    markdown_content = {}
+    for content in (
+        (Path(__file__).parents[1] / "markdown_snippets" / "state.md")
+        .read_text()
+        .split("---")
+    ):
+        slide_name = content.strip("\n").split("\n")[0]
+        markdown_content[slide_name] = "".join(
+            content.strip("\n").split("\n", maxsplit=1)[1:]
+        )
+    return markdown_content
+
+
+def header_setup(slides: list[str]) -> None:
+    n_slides = len(slides)
+    # streamlit allows for column (flexbox) syntax:
+    # our list into st.columns defines width
+    col1, col2, col3 = st.columns(spec=[1, 2, 1])
+
+    with col1:
+        if st.button("⬅️ Previous", disabled=st.session_state.slide == 0):
+            st.session_state.slide -= 1
+            st.rerun()
+
+    with col2:
+        st.markdown(f"#### Slide {st.session_state.slide + 1}", text_alignment="center")
+
+    with col3:
+        if st.button("Next ➡️", disabled=st.session_state.slide == n_slides - 1):
+            st.session_state.slide += 1
+            st.rerun()
+
+
+def state_demo() -> None:
+    """
+    On slide 2 we have two counter buttons,
+    1. keeps it's state in a local variable
+    2. holds it's state in st.session_state
+    """
+    st.divider()
+    st.markdown("### Local variable")
+    with st.expander("code", expanded=False):
+        st.code(
+            """local_count = 0
+if st.button("Local counter (+1)"):
+    local_count += 1
+st.write(f"Local counter: **{local_count}**")"""
+        )
+
+    # local count code
+    local_count = 0
+    if st.button("Local counter (+1)"):
+        local_count += 1
+    st.write(f"Local counter: **{local_count}**")
+
+    st.divider()
+    st.markdown("### Session state")
+    with st.expander("code", expanded=False):
+        st.code(
+            """if "session_count" not in st.session_state:
+    st.session_state.session_count = 0
+if st.button("Session counter (+1)"):
+    st.session_state.session_count += 1
+st.write(f"Session counter: **{st.session_state.session_count}**")
+
+def reset_counter_state() -> None:
+    # This is a callback function.
+    st.session_state.session_count = 0
+
+# using the `on_click` argument we can add the callback to our reset button
+st.button("Reset session counter", on_click=reset_counter_state)"""
+        )
+
+    if "session_count" not in st.session_state:
+        st.session_state.session_count = 0
+    if st.button("Session counter (+1)"):
+        st.session_state.session_count += 1
+    st.write(f"Session counter: **{st.session_state.session_count}**")
+
+    def reset_counter_state() -> None:
+        """This is a callback function."""
+        st.session_state.session_count = 0
+
+    # using the `on_click` argument we can add the callback to our reset button
+    st.button("Reset session counter", on_click=reset_counter_state)
+
+
+def slideshow() -> None:
+    """
+    Orchestrator function for our powerpoint slides.
+    """
+    markdown_content = read_markdown_content()
+    slides = list(markdown_content.keys())
+    header_setup(slides=slides)
+    # horizontal rule to split our header from the body
+    st.divider()
+    st.markdown(markdown_content[str(st.session_state.slide)])
+    if st.session_state.slide == 1:
+        state_demo()
+    if st.session_state.slide == 2:
+        with st.expander("Whats in session_state right now?"):
+            st.write(st.session_state)
+    if st.session_state.slide == 4:
+        if st.button("Try st.rerun!"):
+            st.rerun()
+
+
+if "slide" not in st.session_state:
+    st.session_state["slide"] = 0
+
+slideshow()
